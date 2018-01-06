6ed0109923e560ee196b0d8aa352e269dd5bf4f4
rodmeloufpa <melocbmpa@gmail.com>
2018-01-06 18:16:16 -0300

    Script que define variáveis

---
+++ aula02_Variavel.js
@@ -0,0 +1,17 @@
+/*
+
+Variável é um nome dado a um espaço da memória usado para guardar valores.
+
+Regras para criação de uma variável:
+-Não pode conter palavras chave;
+-Não pode começar com números e caracteres especiais a excessão de (_) sublinhado;
+
+Formato:
+
+var nome_da_variavel = valor
+*/
+
+var x = 2;
+var y = 3.5;
+var nome = "rodrigo";
+console.log(x,y,nome);
